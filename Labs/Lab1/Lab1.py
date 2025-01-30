4e7e65d69db21f2313f3448dcd68e398c38d553e
JessGurn <jthomp61@gmail.com>
2025-01-30 14:27:30 -0700

    Update Lab1.py

--- Labs/Lab1/Lab1.py
+++ Labs/Lab1/Lab1.py
@@ -1,31 +1,21 @@
-
 # # In Class Lab 1
 # Must be uploaded to your Github repository under a "Labs/Lab1" by midnight thursday
 
 # ## Part A:  The Local Standard of Rest
 # Proper motion of Sgr A* from Reid & Brunthaler 2004
-# $\mu = 6.379$ mas/yr 
-# 
-# Peculiar motion of the sun, $v_\odot$ = 12.24 km/s  (Schonrich 2010)
-# 
-# 
+# mu = 6.379$ mas/yr 
+# Peculiar motion of the sun, $v_dot$ = 12.24 km/s  (Schonrich 2010)
 # $v_{tan} = 4.74 \frac{\mu}{\rm mas/yr} \frac{R_o}{\rm kpc} = V_{LSR} + v_\odot$
-# 
-# 
-# ### a)
-# 
+
+######################################################################
+# a)
 # Create a function called VLSR to compute the local standard of rest (V$_{LSR}$).
-# 
 # The function should take as input: the solar radius (R$_o$), the proper motion (mu)
-# and the peculiar motion of the sun in the $v_\odot$ direction.
-# 
+# and the peculiar motion of the sun in the $v_dot$ direction
 # Compute V$_{LSR}$ using three different values R$_o$: 
 # 1. Water Maser Distance for the Sun :  R$_o$ = 8.34 kpc   (Reid 2014 ApJ 783) 
 # 2. GRAVITY Collaboration Distance for the Sun:  R$_o$ = 8.178 kpc   (Abuter+2019 A&A 625)
 # 3. Value for Distance to Sun listed in Sparke & Gallagher : R$_o$ = 7.9 kpc 
-# 
-
-
 
 # Import Modules 
 import numpy as np # import numpy
@@ -50,79 +40,85 @@ def VLSR(Ro, mu=6.379, vsun=12.24*u.km/u.s):
     return VLSR
 
 #different values of the distance to the galactic center
-RoReid = 8.34*u.kpc #Reid + 2014
-RoAbuter = 8.178*u.kpc #Gravity Abuter+2019
-RoSparke = 7.9*u.kpc #Sparke & Gallagher Text
+RoReid = 8.34*u.kpc        #Reid + 2014
+RoAbuter = 8.178*u.kpc     #Gravity Abuter + 2019
+RoSparke = 7.9*u.kpc       #Sparke & Gallagher Text
 
 #computer VLSR using Reid 2014
 VLSR_Reid = VLSR(RoReid)
 print(VLSR_Reid)
+
+#compute VLSR using Abuter
 VLSR_Abuter = VLSR(RoAbuter)
 print(VLSR_Abuter)
+print(np.round(VLSR_Abuter))
+
+#compute VLSR using Sparke
 VLSR_Sparke = VLSR(RoSparke)
 print(VLSR_Sparke)
 
-        
-    
-
-
-# ### b)
-# 
+###############################################################################
+# b)
 # compute the orbital period of the sun in Gyr using R$_o$ from the GRAVITY Collaboration (assume circular orbit)
-# 
 # Note that 1 km/s $\sim$ 1kpc/Gyr
+# orbital period = 2piR/V
+
+def TorbSun(Ro, Vc):
+    """
+    Function that computes the orbital period of the Sun.
+    T = 2piR/V
+
+    Inputs: Ro (astropy qty) distance to the galactic center from sun (kpc)
+            Vc (astropy qty) velocity of the sun in the v direction (km/s)
+
+    Output: T (astropy qty) Orbital period (Gyr)
+    """
+    VkpcGyr = Vc.to(u.kpc/u.Gyr) #convert V to kpc/Gyr
+    T = 2*np.pi*Ro/VkpcGyr
+    return T
+
+VSunPec = 12.24*u.km/u.s         #peculiar motion
+Vsun = VLSR_Abuter + VsunPec     #total motion of sun in v direction
+
+#Orbital Period of the Sun
+T_Abuter = TorbSun(RoAbuter, Vsun)
+print(T_Abuter)
+print(np.round(T_Abuter,3))
 
 
-
-
-
-
-# ### c)
-# 
+##################################################################################
+# c)
 # Compute the number of rotations about the GC over the age of the universe (13.8 Gyr)
+AgeUniverse = 13.8*u.Gyr
+print(AgeUniverse/T_Abuter)
 
-
-
-
-
-
-# ## Part B  Dark Matter Density Profiles
-# 
-# ### a)
+#################################################################################
+# Part B  Dark Matter Density Profiles
+# a)
 # Try out Fitting Rotation Curves 
 # [here](http://wittman.physics.ucdavis.edu/Animations/RotationCurve/GalacticRotation.html)
-# 
-# 
-# ### b)
-# 
-# 
+ 
+
+##############################################################################
+# b)
 # In the Isothermal Sphere model, what is the mass enclosed within the solar radius (R$_o$) in units of M$_\odot$? 
-# 
 # Recall that for the Isothermal sphere :
 # $\rho(r) = \frac{V_{LSR}^2}{4\pi G r^2}$
-# 
 # Where $G$ = 4.4985e-6 kpc$^3$/Gyr$^2$/M$_\odot$, r is in kpc and $V_{LSR}$ is in km/s
-# 
 # What about at 260 kpc (in units of  M$_\odot$) ? 
 
 
 
 
 
-
-# ## c) 
-# 
+##############################################################################
+# c) 
 # The Leo I satellite is one of the fastest moving satellite galaxies we know. 
-# 
-# 
 # It is moving with 3D velocity of magnitude: Vtot = 196 km/s at a distance of 260 kpc (Sohn 2013 ApJ 768)
-# 
 # If we assume that Leo I is moving at the escape speed:
-# 
 # $v_{esc}^2 = 2|\Phi| = 2 \int G \frac{\rho(r)}{r}dV $ 
-# 
-# and assuming the Milky Way is well modeled by a Hernquist Sphere with a scale radius of $a$= 30 kpc, what is the minimum mass of the Milky Way (in units of M$_\odot$) ?  
-# 
+# and assuming the Milky Way is well modeled by a Hernquist Sphere with a scale radius of $a$= 30 kpc, 
+# what is the minimum mass of the Milky Way (in units of M$_\odot$) ?  
 # How does this compare to estimates of the mass assuming the Isothermal Sphere model at 260 kpc (from your answer above)
 
 
